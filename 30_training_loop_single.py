f02d0c48046e20bdec483ea8b29008da5eb8272c
jamesafranke <jfranke@uchicago.edu>
2023-09-20 10:46:49 -0500

    new torchrun scripts

---
+++ 30_training_loop_single.py
@@ -0,0 +1,73 @@
+import os, torch, numpy as np
+from torchvision.io import read_image
+from torch.utils.data import Dataset, DataLoader
+from vit_pytorch import ViT, Dino
+from pathlib import Path
+import time
+
+HOME=str(Path.home())
+
+params = {'batch_size':140, 'shuffle':True, 'num_workers':2}
+
+class CustomDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.data = os.listdir(data_dir)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.data_dir, self.data[idx])
+        img = read_image(img_path)
+        return img / 255 
+
+# CUDA for PyTorch
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch.backends.cuda.matmul.allow_tf32 = True
+
+custom_dataset = CustomDataset(data_dir=f'{HOME}/workspace/hack_team_01/data/processed/patch_1024')
+data_loader = DataLoader(custom_dataset, **params)
+
+vit = ViT(
+    image_size = 1024,
+    patch_size = 32,
+    num_classes = 1000,                # i dont think this matters in self supervised formulation
+    dim = 1024,
+    depth = 6,
+    heads = 8,
+    mlp_dim = 2048
+)
+
+learner = Dino(
+    vit,
+    image_size = 1024,
+    hidden_layer = 'to_latent',        # hidden layer name or index, from which to extract the embedding
+    projection_hidden_size = 256,      # projector network hidden dimension
+    projection_layers = 4,             # number of layers in projection network
+    num_classes_K = 65336,             # output logits dimensions (referenced as K in paper)
+    student_temp = 0.9,                # student temperature
+    teacher_temp = 0.04,               # teacher temperature, needs to be annealed from 0.04 to 0.07 over 30 epochs
+    local_upper_crop_scale = 0.4,      # upper bound for local crop - 0.4 was recommended in the paper 
+    global_lower_crop_scale = 0.5,     # lower bound for global crop - 0.5 was recommended in the paper
+    moving_average_decay = 0.9,        # moving average of encoder - paper showed anywhere from 0.9 to 0.999 was ok
+    center_moving_average_decay = 0.9, # moving average of teacher centers - paper showed anywhere from 0.9 to 0.999 was ok
+).to(device)
+
+opt = torch.optim.Adam(learner.parameters(), lr = 3e-4)
+
+
+t0 = time.time()
+for epoch in range(3):
+    print(epoch)
+    for i, batch in enumerate(data_loader):
+        batch = batch.to(device)
+        loss = learner(batch)
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        learner.update_moving_average() 
+
+t1 = time.time()
+print('time',t1-t0)
---
+++ scripts/torchrun_test.py
@@ -0,0 +1,74 @@
+import os
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torchvision.io import read_image
+from torch.utils.data import Dataset, DataLoader
+from vit_pytorch import ViT, Dino
+from pathlib import Path
+torch.backends.cuda.matmul.allow_tf32 = True
+
+HOME=str(Path.home())
+params = {'batch_size':10, 'shuffle':True, 'num_workers':2}
+
+class CustomDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.data = os.listdir(data_dir)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.data_dir, self.data[idx])
+        img = read_image(img_path)
+        return img / 255 
+
+custom_dataset = CustomDataset(data_dir=f'{HOME}/workspace/hack_team_01/data/processed/patch_1024')
+data_loader = DataLoader(custom_dataset, **params)
+
+vit = ViT(image_size = 1024, patch_size = 32, num_classes = 1000, dim = 1024, depth = 6, heads = 8,  mlp_dim = 2048)
+
+learner = Dino(
+    vit,
+    image_size = 1024,
+    hidden_layer = 'to_latent',        # hidden layer name or index, from which to extract the embedding
+    projection_hidden_size = 256,      # projector network hidden dimension
+    projection_layers = 4,             # number of layers in projection network
+    num_classes_K = 65336,             # output logits dimensions (referenced as K in paper)
+    student_temp = 0.9,                # student temperature
+    teacher_temp = 0.04,               # teacher temp, needs to be annealed from 0.04 to 0.07 over 30 epochs
+    local_upper_crop_scale = 0.4,      # upper bound for local crop - 0.4 was recommended in the paper 
+    global_lower_crop_scale = 0.5,     # lower bound for global crop - 0.5 was recommended in the paper
+    moving_average_decay = 0.9,        # moving average of encoder -anywhere from 0.9 to 0.999 was ok
+    center_moving_average_decay = 0.9, # moving average of teacher centers -anywhere from 0.9 to 0.999 was ok
+)
+
+opt = torch.optim.Adam(learner.parameters(), lr = 3e-4)
+
+def demo_basic():
+    dist.init_process_group("nccl")
+    rank = dist.get_rank()
+    print(f"Start running basic DDP example on rank {rank}.")
+
+    # create model and move it to GPU with id rank
+    device_id = rank % torch.cuda.device_count()
+    model = learner.to(device_id)
+    ddp_model = DDP(model, device_ids=[device_id])
+
+    for epoch in range(3):
+        print(epoch)
+        for batch in data_loader:
+            batch = batch.to(rank)
+            loss = ddp_model(batch)
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+            ddp_model.update_moving_average() 
+
+    dist.destroy_process_group()
+
+if __name__ == "__main__":
+    demo_basic()
